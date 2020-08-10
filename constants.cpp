137f203eabf5c7309da6fc86dbfd27887b63863d
Josef Minařík <minarjos00@gmail.com>
2020-08-10 16:36:54 +0200

    constants file

--- constants.cpp
+++ constants.cpp
@@ -1,4 +1,6 @@
-const int xconsts[] = {0, 100, 500, 600, 400};
-const int yconsts[] = {0, 100, 500, 600, 400};
-const int xthreeconst = 500, xfourconst = 500;
-const int ythreeconst = 600, yfourconst = 700;
+const int fours = 10000;
+const int threes = 1000;
+//                           -  oh o  d   tb  dh  d   th  t   f   fh  ff  five    o  tb
+const int xcombo_scores[] = {0, 4, 8, 40, 40, 15, 30, 25, 50, 50, 25, 100, 100000, 0, 40};
+// const int ocombo_scores[] = {0, 4, 8, 40, 40, 15, 30, 25, 50, 50, 25, 100, 100000, 0, 40};
+const int ocombo_scores[] = {0, 4, 8, 30, 30, 10, 18, 15, 30, 40, 20, 80, 100000, 0, 30};
