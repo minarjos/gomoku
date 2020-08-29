93ca414b29a6711ad48e5fc08e59d8e99f2ba438
Josef Minařík <minarjos00@gmail.com>
2020-08-29 16:02:12 +0200

    changed collors, Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 gomoku: main.cpp position.h position.cpp human.h human.cpp printer.h printer.cpp ai.h ai.cpp game.h game.cpp player.h
-	g++ -o main main.cpp -O3 -lcurses
+	g++ -o main main.cpp -O3 -lcurses -Wall
 
 clean:
 	rm main
--- printer.cpp
+++ printer.cpp
@@ -7,15 +7,23 @@ void init()
   initscr();
   keypad(stdscr, TRUE);
   start_color();
-  init_pair(0, COLOR_RED, COLOR_BLACK);
-  init_pair(1, COLOR_RED, COLOR_BLACK);
-  init_pair(2, COLOR_BLUE, COLOR_BLACK);
-  init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+  init_color(COLOR_RED, 1000, 0, 0);
+  init_color(COLOR_BLUE, 0, 0, 1000);
+  init_color(COLOR_MAGENTA, 1000, 700, 0);
+  init_color(COLOR_WHITE, 1000, 1000, 1000);
+  init_color(COLOR_BLACK, 0, 0, 0);
+  init_pair(0, COLOR_BLACK, COLOR_WHITE);
+  init_pair(1, COLOR_RED, COLOR_WHITE);
+  init_pair(2, COLOR_BLUE, COLOR_WHITE);
+  init_pair(3, COLOR_MAGENTA, COLOR_WHITE);
+  init_pair(4, COLOR_BLACK, COLOR_WHITE);
 }
 
 void print_row(const position &p, int row)
 {
+  attron(COLOR_PAIR(4));
   printw("    +");
+  attroff(COLOR_PAIR(4));
   for (int i = 0; i < p.N; i++)
   {
     if ((hilight.x == row || hilight.x == row + 1) && hilight.y == i + 1)
@@ -25,22 +33,33 @@ void print_row(const position &p, int row)
       attroff(COLOR_PAIR(3));
     }
     else
+    {
+      attron(COLOR_PAIR(4));
       printw(" - ");
+      attroff(COLOR_PAIR(4));
+    }
+    attron(COLOR_PAIR(4));
     printw("+");
+    attroff(COLOR_PAIR(4));
   }
-  printw("\n");
+  attron(COLOR_PAIR(4));
+  printw("    \n");
+  attroff(COLOR_PAIR(4));
 }
 
 void print_numbers(const position &p)
 {
+  attron(COLOR_PAIR(4));
   printw("    ");
   for (int i = 1; i <= p.N; i++)
     printw("%3d ", i);
-  printw("\n");
+  printw("     \n");
+  attroff(COLOR_PAIR(4));
 }
 
 void printsep(int i, int j)
 {
+  attron(COLOR_PAIR(4));
   if (hilight.x == i && (hilight.y == j || hilight.y == j + 1))
   {
     attron(COLOR_PAIR(3));
@@ -49,6 +68,7 @@ void printsep(int i, int j)
   }
   else
     printw("|");
+  attroff(COLOR_PAIR(4));
 }
 
 void print(const position &p)
@@ -58,9 +78,12 @@ void print(const position &p)
   print_row(p, 0);
   for (int i = 1; i <= p.N; i++)
   {
+    attron(COLOR_PAIR(4));
     printw("%3d ", i);
     printsep(i, 0);
+    attron(COLOR_PAIR(4));
     printw(" ");
+    attroff(COLOR_PAIR(4));
     for (int j = 1; j <= p.N; j++)
     {
       if (!p.moves.empty() && i == p.moves.back().x && j == p.moves.back().y)
@@ -69,20 +92,31 @@ void print(const position &p)
         printw("%c", symbol[p.board[i][j]]);
         attroff(COLOR_PAIR(3));
       }
+      else if(symbol[p.board[i][j]] == ' ')
+      {
+        attron(COLOR_PAIR(4));
+        printw("%c", symbol[p.board[i][j]]);
+        attroff(COLOR_PAIR(4));
+      }
       else
       {
         attron(COLOR_PAIR(p.board[i][j]));
         printw("%c", symbol[p.board[i][j]]);
         attroff(COLOR_PAIR(p.board[i][j]));
       }
+      attron(COLOR_PAIR(4));
       printw(" ");
       printsep(i, j);
+      attron(COLOR_PAIR(4));
       if (j < p.N)
         printw(" ");
+      attroff(COLOR_PAIR(4));
     }
+    attron(COLOR_PAIR(4));
     printw("%3d ", i);
     printw("\n");
     print_row(p, i);
+    attroff(COLOR_PAIR(4));
   }
   print_numbers(p);
   refresh();
